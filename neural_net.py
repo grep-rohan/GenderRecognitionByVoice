c2d2bbb52391e1a00281075af2bc965c0f761e53
Rohan Sharma <racy.rohan@gmail.com>
2017-04-12 00:00:19 +0530

    Added prepreocess and neural_net

---
+++ neural_net.py
@@ -0,0 +1,64 @@
+import warnings
+
+from pandas import Series, DataFrame
+from sklearn.neural_network import MLPClassifier
+
+import preprocess
+
+warnings.filterwarnings("ignore")
+
+attributes = ('meanfreq', 'sd', 'median', 'Q25', 'Q75', 'IQR')
+
+if __name__ == '__main__':
+    data = preprocess.read()
+    train, test = preprocess.split(data)
+    training_inputs = [[train.iloc[index][attribute] for attribute in attributes] for index in range(len(train))]
+    training_outputs = [train.iloc[i]['label'] for i in range(len(train))]
+    testing_inputs = [[test.iloc[index][attribute] for attribute in attributes] for index in range(len(test))]
+    testing_outputs = [test.iloc[i]['label'] for i in range(len(test))]
+
+    neural_net = MLPClassifier(hidden_layer_sizes=(1,), activation='identity', solver='adam',
+                               learning_rate='adaptive', max_iter=2000, verbose=True)
+    neural_net.fit(training_inputs, training_outputs)
+    preprocess.visualize(Series(neural_net.loss_curve_))
+
+    tests = DataFrame(columns=('Actual', 'Predicted'))
+    index = 0
+    for testing_input, testing_output in zip(testing_inputs, testing_outputs):
+        predicted_output = neural_net.predict(testing_input)
+        tests.loc[index] = [testing_output, predicted_output[0]]
+        index += 1
+
+    true_pos = true_neg = false_pos = false_neg = 0
+    for actual, predicted in zip(tests['Actual'], tests['Predicted']):
+        if actual == predicted == 0:
+            true_neg += 1
+        elif actual == predicted == 1:
+            true_pos += 1
+        elif actual == 0 != predicted:
+            false_neg += 1
+        else:
+            false_pos += 1
+
+    accuracy = precision = recall = fallout = specificity = 0
+    try:
+        accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg)
+    except ZeroDivisionError:
+        pass
+    try:
+        precision = true_pos / (true_pos + false_pos)
+    except ZeroDivisionError:
+        pass
+    try:
+        recall = true_pos / (true_pos + false_neg)
+    except ZeroDivisionError:
+        pass
+    try:
+        specificity = true_neg / (true_neg + false_pos)
+    except ZeroDivisionError:
+        pass
+
+    print('\nAccuracy    = %.2f%%' % (accuracy*100))
+    print('Precision   = %.2f%%' % (precision*100))
+    print('Recall      = %.2f%%' % (recall*100))
+    print('Specificity = %.2f%%' % (specificity*100))
---
+++ preprocess.py
@@ -0,0 +1,62 @@
+from math import floor
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
+def read(filename='voice.csv'):
+    """
+    Read data from file.
+    
+    :param filename:  Name of file containing data.
+    :return: data. (pandas DataFrame)
+    """
+    data = None
+    try:
+        data = pd.read_csv(filename)  # read data from csv file
+        print('Reading data')
+        data['label'] = LabelEncoder().fit_transform(data['label'])  # encode class label
+        data = data.sample(frac=1).reset_index(drop=True)  # randomize order
+    except FileNotFoundError:
+        print('File not found!')
+
+    return data
+
+
+def split(data, train_percent=.6):
+    """
+    Split data into training and testing data.
+
+    :param data: The data to be split.
+    :param train_percent: Percentage of data which is training. def = 0.6
+    :return: Tuple containing training and testing data.
+    """
+    train_data = data.iloc[0:floor(train_percent * len(data))]
+    test_data = data.iloc[floor(train_percent * len(data)):]
+
+    return train_data, test_data
+
+
+def visualize(data, style='ggplot', graph_type='line'):
+    """
+    Visualize data.
+
+    :param data: Data to visualize. (pandas dataframe)
+    :param style: matplotlib style. def = 'ggplot'
+    :param graph_type: Graph type ('line' or 'area'). def = 'line'
+    :return: None
+    """
+    try:
+        plt.style.use(style)
+    except OSError:
+        print('\nInvalid style!\nUsing ggplot\n')
+        plt.style.use('ggplot')
+    if graph_type == 'line':
+        data.plot()
+    elif graph_type == 'area':
+        data.plot.area(stacked=False)
+    else:
+        print('\nInvalid type!\nUsing line')
+        data.plot()
+    plt.show()
